e72ba112ae343074026eed565f1173337f075e8a
maershaa <valeria.work2022@gmail.com>
2023-12-02 10:03:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
-import { App } from 'App';
-import 'index.css';
+import { App } from '../src/App';
+import '../src/App.module.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
