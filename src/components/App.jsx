6090ef6458c15a51f3381be710b8e7d706586a3c
maershaa <valeria.work2022@gmail.com>
2023-12-02 10:16:34 +0200

    initialisation commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from 'redux/operation';
+import ContactForm from 'components/ContactForm/ContactForm';
+import ContactList from 'components/Contacts/ContactsList';
+import Filter from 'components/Filter/Filter';
+import css from './App.module.css';
+
+const App = () => {
+  // Локальное состояние для отслеживания активной вкладки
+  const [activeTab, setActiveTab] = useState('form');
+  const dispatch = useDispatch();
+
+  // Функция для изменения активной вкладки
+  const handleTabChange = tab => {
+    setActiveTab(tab);
+  };
+
+  // !размещено тут чтобы если списко контатков не изменился, то и запрос по 100 раз делать не нужно
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+
+      {/* Компоненты для переключения между вкладками */}
+      <div className={css.tabs}>
+        {/* Кнопка для переключения на форму добавления контакта */}
+        <button
+          onClick={() => handleTabChange('form')}
+          className={activeTab === 'form' ? css.activeTab : ''}
+        >
+          Contact Form
+        </button>
+
+        {/* Кнопка для переключения на фильтр и список контактов */}
+        <button
+          onClick={() => handleTabChange('list')}
+          className={activeTab === 'list' ? css.activeTab : ''}
+        >
+          Contact List
+        </button>
+      </div>
+
+      {/* Контент соответствующей вкладки */}
+      <div className={css.tabContent}>
+        {/* Если активная вкладка - форма добавления контакта */}
+        {activeTab === 'form' && <ContactForm />}
+
+        {/* Если активная вкладка - список контактов */}
+        {activeTab === 'list' && (
+          <div className={css.contactsAndSearchWrapper}>
+            <Filter />
+            <ContactList />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
-import { App } from '../src/App';
-import '../src/App.module.css';
+import App from 'components/App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
